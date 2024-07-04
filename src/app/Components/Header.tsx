ca8f0df5492dc89b926ff78e486abfcc75808d5b
Lemuel Leogene Reyes <lemuelp.reyes@gmail.com>
2024-07-04 21:32:23 +0800

    add ellipsis icon button

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import logo_light from "../../../public/logo-dark.svg";
 import logo_dark from "../../../public/logo-light.svg";
+import more_icon from "../../../public/icon-vertical-ellipsis.svg";
 
 export default function Header() {
   return (
@@ -11,9 +12,13 @@ export default function Header() {
       </div>
       <div className="flex flex-row justify-between items-center w-full border-b border-linesLight p-4">
         <h1 className="text-xl font-bold">Platform Launch</h1>
-        <div className="">
-          <button className="bg-mainPurple p-4 font-bold text-md text-white rounded-3xl">+ Add New Task</button>
-          <button>More</button>
+        <div className="flex gap-8">
+          <button className="bg-mainPurple p-4 font-bold text-md text-white rounded-3xl">
+            + Add New Task
+          </button>
+          <button>
+            <Image src={more_icon} alt="ellipsis_icon" />
+          </button>
         </div>
       </div>
     </header>
