d4b9bdb3aa554e96b6f3090a6f2c674d223d82b8
Lemuel Leogene Reyes <lemuelp.reyes@gmail.com>
2024-07-15 21:29:45 +0800

    get board information from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import MainBoard from "./Components/MainBoard";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 import { ThemeProvider } from "./ThemeContext";
+import prisma from "./utils/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const boards = await prisma.board.findMany();
+  console.log("Boards: ", boards);
+
   return (
     <div className="flex flex-col h-screen">
       <ThemeProvider>
--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,12 +1,20 @@
+// code snippet from prisma.io
+// URL: https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = () => {
+const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClient>;
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-export const prisma = globalThis.prisma ?? prismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
